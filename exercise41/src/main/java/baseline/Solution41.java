7ac360342f4aaf3a77a2931b64a9c53a434d7c1e
jmazor <mazorj@pm.me>
2021-10-15 22:04:18 -0400

    41 pseudo

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,16 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
+
+public class Solution41 {
+    public static void main(String[] args) {
+        // create a file object
+        // try catch
+        // while we have a next line put it into a treemap
+        // print
+    }
+}
