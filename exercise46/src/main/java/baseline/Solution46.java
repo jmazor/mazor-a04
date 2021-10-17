8ee5c5256e183bdaa3887e7e09618a4839212c17
jmazor <mazorj@pm.me>
2021-10-17 16:46:32 -0400

    46 Pseudo

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,34 @@
+package baseline;
+
+import java.io.File;
+import java.util.Map;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Justin Mazor
+ */
+
+
+public class Solution46 {
+
+    Map<String, Integer> createHistogram(File file) {
+        // read through a text file
+        // check to see if word is in map
+        // if not add it to map and increase value
+        // if it is increase value
+    }
+
+    static void printMap(Map<String, Integer> data) {
+        // print stars * Integer
+    }
+
+    static void sortMap(Map<String, Integer> data) {
+        // not sure if this will be needed
+        // if needed create a new map and add the next greatest count to back of it
+        // sort by value
+    }
+
+    public static void main(String[] args) {
+        // call create Histogram
+    }
+}
