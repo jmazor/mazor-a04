2ace0331d4e1d64151cd3c75a16f1cc20d7e9a4a
jmazor <mazorj@pm.me>
2021-10-17 18:21:32 -0400

    45 pseudo

---
+++ exercise45/src/main/java/baseline/FindNReplace.java
@@ -0,0 +1,20 @@
+package baseline;
+
+import java.io.File;
+
+public class FindNReplace {
+    private String;
+
+    public FindNReplace (File file) {
+        // try catch
+        // gets input and returns a string
+    }
+
+    public String replaceWords(String removeWord, String addWord) {
+        // call replaceAll funciton
+    }
+
+    public void printString() {
+        // try catch to write to file
+    }
+}
---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,14 @@
+package baseline;
+
+import java.io.File;
+
+public class Solution45 {
+
+    public static void main(String[] args) {
+        // init FindNReplace
+        // call getInput
+        // call replaceWords
+        // ask for new file name
+        // print to file
+    }
+}
