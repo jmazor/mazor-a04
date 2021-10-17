b7badf09eba0692d0d673ba3bd036c9f4f299f58
jmazor <mazorj@pm.me>
2021-10-16 23:24:01 -0400

    42 Pseudo

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,28 @@
+package baseline;
+
+import java.util.List;
+
+public class Solution42 {
+
+    public List<List<String>> readInput(){
+        // try catch read input
+        // call csv parser
+        // input into a list of lists
+    }
+
+    public List<String> csvParse(String) {
+        // seperate line by strings
+        // add each new parsed string to List
+        // return when hit end of line
+    }
+
+    public static void printList (List<List<String>>) {
+        // for each item in list
+        // print each item of list
+    }
+
+    public static void main(String[] args) {
+        // call readInput
+        // call printList
+    }
+}
