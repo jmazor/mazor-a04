285d5a665f4dc0b63752ad8380e3002f5d7e25f1
jmazor <mazorj@pm.me>
2021-10-17 19:00:04 -0400

    44 pseudo

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,32 @@
+package baseline;
+
+public class Solution44 {
+    public static void main(String[] args) {
+        // init ProductSearch
+        // call askSearch
+    }
+}
+
+public class MyProduct {
+    //String Name
+    //Double Price
+    //Int Quanitity
+
+
+}
+
+public class ProductSearch {
+    // List of MyProduct
+
+    // askSearchMethod
+    //ask for product name until a result is achieved
+    // call actualSearch
+
+    // parseData
+    // use gson to parse data
+
+    // actualSearch
+    // loop through list testing if Name equals input
+    // if equals return MyProduct
+}
+
